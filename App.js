b4b4ce9b872bcf34a98693c9cfd7a3e90d6ff65b
Heli-DL <heli.delorme@outlook.com>
2023-01-17 20:16:15 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+  const [result, setResult] = useState(0);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const sumNumbers = () => {  
+    setResult(number1 + number2);
+    setText(number1 + ' + ' + number2 + ' = ' + (number1 - number2));
+    setData([...data, {key: text}]);
+    setNumber1('');
+    setNumber2('');
+  }
+
+  const substract = () => {  
+    setResult(number1 - number2);
+    setText(number1 + ' - ' + number2 + ' = ' + (number1 - number2));
+    setData([...data, {key: text}]);
+    setNumber1('');
+    setNumber2('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputbox}>
+        <Text>Result: {result}</Text>
+        <TextInput style={{width:200, borderColor: 'gray', borderWidth:1}}onChangeText={number1 => setNumber1(Number(number1))}  value={number1} keyboardType={'numeric'}/>
+        <TextInput style={{width:200, borderColor: 'gray', borderWidth:1}}onChangeText={number2 => setNumber2(Number(number2))}  value={number2} keyboardType={'numeric'}/>
+      </View>
+      <View style={styles.buttons}> 
+        <View style={styles.button}>
+          <Button onPress={sumNumbers}title="+" />
+        </View>
+        <View style={styles.button}>
+          <Button onPress={() => {setResult(number1 - number2); substract();}}title="-" />
+        </View>
+      </View>
+      <View style={styles.listbox}>
+        <Text>History</Text>
+        <FlatList 
+          data={data}
+          renderItem={({item}) =><Text>{item.key}</Text>}  
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+      <StatusBar style="auto" /> 
     </View>
   );
 }
@@ -17,4 +60,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttons: {
+    flexDirection: 'row',
+    margin: 10,
+  },
+  button: {
+    width: 30,
+    margin: 10
+  },
+  inputbox: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  listbox: {
+    flex: 3,
+    justifyContent: 'flex-start',
+  }
 });
